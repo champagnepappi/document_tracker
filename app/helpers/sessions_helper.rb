429f5a0f9bf4327cc27789fc3455c9dead66a905
kevin <kevinochieng548@gmail.com>
2016-07-28 20:47:51 +0300

    remember user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,15 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #remember user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.signed[:user_id] = { value: user.id,
+                                expires: 7.days.from_now.utc}
+    cookies[:remember_token] = { value: user.remember_token,
+                                   expires: 7.days.from_now.utc}
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
