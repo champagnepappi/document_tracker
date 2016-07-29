126ade3dfb90755c290a6c1691f2ac5ee723137f
kevin <kevinochieng548@gmail.com>
2016-07-29 09:06:46 +0300

    raise exception in untested branch

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,15 +7,16 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.signed[:user_id] = { value: user.id,
-                                expires: 7.days.from_now.utc}
+                                 expires: 7.days.from_now.utc}
     cookies[:remember_token] = { value: user.remember_token,
-                                   expires: 7.days.from_now.utc}
+                                 expires: 7.days.from_now.utc}
   end
 
   def current_user
     if (user_id = session[:user_id])
-    @current_user ||= User.find_by(id: user_id)
-    else if (user_id = cookies.signed[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -36,6 +37,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
