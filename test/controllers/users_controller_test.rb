de43c3b26ab83fb85835820776ae84d581aed397
kevin <kevinochieng548@gmail.com>
2016-07-29 10:20:08 +0300

    test that edit and update are protected when not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,30 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:santos)
+  end
+
   test "should get new" do
     get new_user_path
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user), params: {id: @user}
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params:{ id: @user ,
+      user: {
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
 end
