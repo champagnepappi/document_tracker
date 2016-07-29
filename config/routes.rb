2e903a9beb651606ecfd52e8181404a3aeb7982d
kevin <kevinochieng548@gmail.com>
2016-07-29 12:39:52 +0300

    add resource for account activations

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
 
   delete 'logout' => 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
