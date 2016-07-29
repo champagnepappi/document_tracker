317bcbc3c4244263d24e045541729c8fd0008848
kevin <kevinochieng548@gmail.com>
2016-07-29 10:02:49 +0300

    require only logged in user to edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit,:update]
+
   def new
     @user = User.new
   end
@@ -24,6 +26,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find_by(id: params[:id])
     if @user.update_attributes(permit_params)
+      flash[:success] = "Your profile was successfully updated"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -32,5 +36,12 @@ class UsersController < ApplicationController
   private
   def permit_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please login to continue"
+      redirect_to login_url
+    end
   end
 end
