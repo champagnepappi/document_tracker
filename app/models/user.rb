a6ea4467c0b56de68f9d59307892f341239bced2
kevin <kevinochieng548@gmail.com>
2016-07-29 16:27:49 +0300

    define generalized authenticated method

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,9 +34,10 @@ class User < ApplicationRecord
     self.activation_digest = User.digest(activation_token)
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
