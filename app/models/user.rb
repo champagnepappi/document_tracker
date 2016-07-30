a95a525c8103feff0137607cc9165650a22c8d54
kevin <kevinochieng548@gmail.com>
2016-07-30 15:46:53 +0300

    ensure password is reset earlier than two hours

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,10 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   def activate
     update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
