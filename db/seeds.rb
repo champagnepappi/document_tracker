c19e5d00a5f56615a6ddf35f528f5b4df29e57f0
kevin <kevinochieng548@gmail.com>
2016-08-01 17:45:05 +0300

    sample documents

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 User.create!(name: "Kevin Einstein",
-            email: "kevinochieng548@gmail.com",
-            password: "password",
-            password_confirmation: "password",
-            activated: true,
-            activated_at: Time.zone.now)
+
+             email: "kevinochieng548@gmail.com",
+             password: "password",
+             password_confirmation: "password",
+             activated: true,
+             activated_at: Time.zone.now)
 
 
 20.times do |n|
@@ -11,25 +12,27 @@ User.create!(name: "Kevin Einstein",
   email = Faker::Internet.email
   password = "password"
   User.create!(name: name,
-              email: email,
-              password: password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+               email: email,
+               password: password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
 
-30.times do |n|
+users = User.order(:created_at).take(10)
+20.times do 
   title = Faker::Book.title
   author = Faker::Name.name
   departments = ["Success", "Life","Sports","Finance","Inspiration","Business"]
   department = departments[rand(6)]
   link = Faker::Internet.url
   content = Faker::Lorem.paragraph
-  Document.create!(title: title,
-                   author: author,
-                  department: department,
-                  link: link,
-                  content: content)
+  users.each { |user| user.documents.create!(title: title,
+                                             author: author,
+                                             department: department,
+                                             link: link,
+                                             content: content)
+  }
 end
 
 
