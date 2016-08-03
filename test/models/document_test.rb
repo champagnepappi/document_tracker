f6649b266dc4ef96d9960a579b9774d4ded511ad
kevin <kevinochieng548@gmail.com>
2016-08-03 21:10:36 +0300

    test presence of title,author,department and content

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -4,13 +4,21 @@ class DocumentTest < ActiveSupport::TestCase
   def setup
     @user = users(:santos)
     @document = @user.documents.build(author: "shaniz",
-                                     title: "anti hay",
-                                     link: "http://www.wfsdas.com",
-                                     department: "Success",
-                                     content: "hbvfwsijhfiqjoahiuqhiudqiu")
+                                      title: "anti hay",
+                                      link: "http://www.wfsdas.com",
+                                      department: "Success",
+                                      content: "hbvfwsijhfiqjoahiuqhiudqiu")
   end
   test "document should have user_id" do
     @document.user_id = nil
+    assert_not @document.valid?
+  end
+
+  test "author,title, department and content should be present" do
+    @document.title = " "
+    @document.author = " "
+    @document.department = ""
+    @document.content = ""
     assert_not @document.valid?
   end
 end
