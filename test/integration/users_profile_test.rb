802df1de65b603438df8a5ffbcd311afa241849f
kevin <kevinochieng548@gmail.com>
2016-08-06 20:54:34 +0300

    test users profile after adding and deleting a document

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,13 +1,16 @@
 require 'test_helper'
 
-class DocumentsAdditionTest < ActionDispatch::IntegrationTest
+class UsersProfileTest < ActionDispatch::IntegrationTest
+
   def setup
     @user = users(:santos)
     @document = documents(:one)
   end
-  test "add document with invalid params" do
+
+  test "users profile" do
     log_in_as(@user)
-    get new_document_path
+    get user_path(@user)
+    #invalid document creation
     assert_no_difference 'Document.count' do
       post documents_path params: {
         document: {
@@ -20,12 +23,7 @@ class DocumentsAdditionTest < ActionDispatch::IntegrationTest
         }
       }
     end
-    assert_template 'new'
-  end
-
-  test "add document with valid params" do
-    log_in_as(@user)
-    get new_document_path
+    #valid document creation
     assert_difference 'Document.count', 1 do
       post documents_path params: {
         document: {
@@ -38,6 +36,17 @@ class DocumentsAdditionTest < ActionDispatch::IntegrationTest
         }
       }
     end
-    assert_redirected_to @user
+    assert_redirected_to user_path(@user)
+    follow_redirect!
+    #delete a document
+    assert_select 'a', text: 'delete'
+    document = @user.documents.first
+    assert_difference 'Document.count', -1 do
+      delete document_path(document)
+    end
+    assert_redirected_to user_path(@user)
+    #profile for another user
+    get user_path(users(:shaz))
+    assert_select 'a', text: 'delete', count: 0
   end
 end
