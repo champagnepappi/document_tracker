8145504c471865f6dcf19c3c6441b55646d9d959
kevin <kevinochieng548@gmail.com>
2016-08-17 15:21:01 +0300

    change background color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804130751) do
+ActiveRecord::Schema.define(version: 20160816064105) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "author"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20160804130751) do
     t.datetime "updated_at", null: false
     t.string   "tag"
     t.index ["user_id"], name: "index_documents_on_user_id"
+  end
+
+  create_table "search_suggestions", force: :cascade do |t|
+    t.string   "term"
+    t.integer  "popularity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
