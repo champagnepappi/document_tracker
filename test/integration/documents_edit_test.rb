f9d666cf6e534e9c15e1b6a13e0dacc66fa683a4
kevin <kevinochieng548@gmail.com>
2016-08-19 08:49:23 +0300

    modify documents new and edit pages

--- test/integration/documents_edit_test.rb
+++ test/integration/documents_edit_test.rb
@@ -4,23 +4,24 @@ class DocumentsEditTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:santos)
+
     @document = documents(:one)
   end
 
-  # test "unsuccessful edit" do
-  #   log_in_as(@user)
-  #   get edit_document_path(@document)
-  #   patch document_path(@document), params: {
-  #     document: {
-  #       title: "",
-  #       author: "",
-  #       department: "",
-  #       link: "alnflka",
-  #       tag: "",
-  #       content: ""
-  #     }
-  #   }
-  #   assert_template 'documents/edit'
-  #   assert flash.empty?
-  # end
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_document_path(@document)
+    patch document_path(@document), params: {
+      document: {
+        title: "",
+        author: "",
+        department: "Sports",
+        link: "alnflka",
+        tag: "",
+        content: ""
+      }
+    }
+    assert_template 'edit'
+    assert flash.empty?
+  end
 end
