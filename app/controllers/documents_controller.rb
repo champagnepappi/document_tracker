aeb05b0b10baad01c42876e927d82dc99042c4e3
kevin <kevinochieng548@gmail.com>
2016-08-19 08:43:22 +0300

    modify before filter so that current user edits and update doc

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
   before_action :logged_in_user
-  before_action :correct_user, only: [:edit, :update]
+   before_action :current_user, only: [:edit, :update]
 
   def index
     if params[:department]
@@ -65,8 +65,4 @@ class DocumentsController < ApplicationController
     end
   end
   
-  def correct_user
-    @user = User.find_by(id: params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
 end
