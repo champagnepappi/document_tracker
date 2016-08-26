d548c0873269dd222262d9ccc630f82579e0ba6e
kevin <kevinochieng548@gmail.com>
2016-08-26 15:02:46 +0300

    test valid message submission

--- db/schema.rb
+++ db/schema.rb
@@ -35,13 +35,6 @@ ActiveRecord::Schema.define(version: 20160822185157) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "search_suggestions", force: :cascade do |t|
-    t.string   "term"
-    t.integer  "popularity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   def setup
     @common_title = "| DocumentTracker"
+    @user = users(:santos)
   end
   test "should get home" do
     get :home
@@ -11,6 +12,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get contact" do
+    log_in_as(@user)
     get :contact
     assert_response :success
     assert_select "title", "Contact #{@common_title}"
