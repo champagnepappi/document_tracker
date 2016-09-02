b37e01e6e32d4713ea2516667d8c62291c76e145
kevin <kevinochieng548@gmail.com>
2016-09-02 16:47:07 +0300

    configure action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,18 +26,11 @@ Rails.application.configure do
   config.assets.compile = false
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
   host = "doc-tracker.herokuapp.com"
     config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-     address: "smtp.sendgrid.net",
-     port: "587",
-     authentication: "plain",
-     user_name: ENV['SENDGRID_USERNAME'],
-     password: ENV['SENDGRID_PASSWORD'],
-     domain: "heroku.com",
-     enable_starttls_auto: true
-  }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
